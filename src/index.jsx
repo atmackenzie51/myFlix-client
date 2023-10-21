d5ae4d1749f02041e71f17f320a1dbbedc786a9c
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2023-10-21 18:15:41 -0400

    updated name
    fixed naming and files

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
 import { createRoot } from 'react-dom/client';
 
+import { MainView } from './components/main-view/main-view';
+
 //Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 //Main component
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div> Good morning </div>
-    </div>
-  );
+const MyFlixApp = () => {
+  return <MainView />;
 };
 
 //Finds the root of the app
@@ -17,4 +15,4 @@ const container = document.querySelector("#root");
 const root = createRoot(container);
 
 //Tells React ti render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<MyFlixApp />);
