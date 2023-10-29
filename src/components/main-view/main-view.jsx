edeeab227b7cd8f678ab6c81db33acaed617ecbc
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2023-10-29 18:45:02 -0400

    updated to pull movie info from API
    removed the manually coded movies and updated with movies in Heroku API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,36 +1,31 @@
 import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
-export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Lord of the Rings: The Fellowship of the Ring",
-      image: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Lord_of_the_Rings%2C_TFOTR_%282001%29.jpg",
-      genre: "Adventure",
-      director: "Peter Jackson"
-    },
-    {
-      id: 2,
-      title: "The Lord of the Rings: The Two Towers",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/f/fc/The_Lord_of_the_Rings%2C_T2T_%282002%29.jpg",
-      genre: "Adventure",
-      director: "Peter Jackson"
-    },
-    {
-      id: 3,
-      title: "The Lord of the Rings: The Return of the King",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/2/23/The_Lord_of_the_Rings%2C_TROTK_%282003%29.jpg",
-      genre: "Adventure",
-      director: "Peter Jackson"
-    }
 
-  ]);
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://movieflix-app-d827ee527a6d.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image: doc.image,
+            genre: doc.genre,
+            director: doc.director
+          };
+        });
+
+        setMovies(moviesFromApi)
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
