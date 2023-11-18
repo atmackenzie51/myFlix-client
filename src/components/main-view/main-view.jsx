70db577e24b311762d77c66f24c15c358472e929
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2023-11-18 14:41:44 -0500

    updated main-view
    with login and sign up functions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,29 +2,58 @@ import { useState } from "react";
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    fetch("https://movieflix-app-d827ee527a6d.herokuapp.com/movies")
-      .then((response) => response.json())
-      .then((data) => {
-        const moviesFromApi = data.map((movie) => {
-          return {
-            id: movie._id,
-            title: movie.Title,
-            genre: movie.Genre,
-            director: movie.Director
-          };
-        });
+    if (!token) {
+      return;
+    }
 
-        setMovies(moviesFromApi);
+    fetch("https://movieflix-app-d827ee527a6d.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((movies) => {
+        setMovies(movies);
       });
-  }, []);
+  }, [token]);
+  /*        const moviesFromApi = data.map((movie) => {
+            return {
+              id: movie._id,
+              title: movie.Title,
+              genre: movie.Genre,
+              director: movie.Director
+            };
+          });
+  
+          setMovies(moviesFromApi);
+        });
+    }, []);*/
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -47,6 +76,7 @@ export const MainView = () => {
           }}
         />
       ))}
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}> Logout </button>
     </div>
   );
 };
