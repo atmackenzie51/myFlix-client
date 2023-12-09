3677974fbd90e48478fcf6aedb11e65183152c12
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2023-12-08 19:17:54 -0500

    updated to match server side

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -51,7 +51,7 @@ export const SignupView = () => {
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required
-                    minLength="3"
+                    minLength="5"
                     placeholder="username must be at least 3 characters"
                   />
                 </Form.Group>
